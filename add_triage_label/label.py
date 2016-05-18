8aa47549aef64cceaeb500c2492cdb69b85e4889
Andy McKay <amckay@mozilla.com>
2016-05-17 21:07:23 -0700

    paginate through the links

--- add_triage_label/label.py
+++ add_triage_label/label.py
@@ -1,8 +1,9 @@
 import datetime
 import logging
 import os
-import sys
+import re
 import requests
+import sys
 
 log = logging.getLogger()
 handler = logging.StreamHandler(sys.stderr)
@@ -33,6 +34,14 @@ developers = [
     'muffinresearch',
 ]
 
+
+issues = []
+
+def parse_link_headers(header):
+    rx = '\<(.*?)>; rel="(\w+)"(?:,)'
+    return dict([(v, k) for k, v in re.findall(rx, header)])
+
+
 def list_issues(location):
     # https://developer.github.com/v3/search/
     # Note this defaults to open pull requests.
@@ -40,14 +49,20 @@ def list_issues(location):
     labels = 'state:open+type:issue+-label:triaged+repo:{}'.format(location)
     # Because github fails when you pass these as params and escape them.
     url = url + '?q={}&sort:created'.format(labels)
+    _list_issues(url)
+
+
+def _list_issues(url):
+    log.info('Calling: {}'.format(url))
     res = requests.get(url)
     res.raise_for_status()
-    issues = []
     for issue in res.json()['items']:
         if issue['user']['login'] in developers:
             issues.append(issue)
 
-    return issues
+    next_url = parse_link_headers(res.headers.get('link')).get('next')
+    if next_url:
+        _list_issues(next_url)
 
 
 def triage_issue(location, issue):
@@ -67,7 +82,7 @@ if __name__ == '__main__':
     location = sys.argv[1]
     assert '/' in location
     log.info('Checking: {}'.format(location))
-    issues = list_issues(location)
+    list_issues(location)
     for issue in issues:
         log.info('Automatic triage of: {}'.format(issue['number']))
         triage_issue(location, issue)
