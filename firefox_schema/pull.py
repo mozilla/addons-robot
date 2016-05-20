7f9aa2f9f9e2842fa26ad024a0bda6f3cee6a653
Andy McKay <amckay@mozilla.com>
2016-05-20 12:01:32 -0700

    wip

---
+++ firefox_schema/pull.py
@@ -0,0 +1,121 @@
+import contextlib
+import datetime
+import logging
+import os
+import requests
+import shutil
+import sys
+import time
+
+log = logging.getLogger('pull')
+handler = logging.StreamHandler(sys.stderr)
+log.addHandler(handler)
+log.setLevel(logging.DEBUG)
+
+project_root = '../other_projects'
+source_root = 'gecko-dev'
+source_repo = 'git@github.com:mozilla/gecko-dev.git'
+dest_root = 'webextension-schema'
+dest_repo = 'https://addons-robot@github.com/andymckay/webextension-schema.git'
+
+dir_mapping = [
+    [
+        os.path.join(source_root, 'toolkit/components/extensions/schemas'),
+        os.path.join(dest_root, 'gecko/toolkit'),
+    ],
+    [
+        os.path.join(source_root, 'browser/components/extensions/schemas'),
+        os.path.join(dest_root, 'gecko/browser'),
+    ]
+]
+
+GITHUB_USERNAME = os.getenv('GITHUB_USERNAME')
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+
+
+def git(cmd):
+    log.debug('git cmd: {}'.format(cmd))
+    return os.popen('git {}'.format(cmd)).readlines()
+
+
+def make_pull_request(commit, uid):
+    url = 'https://api.github.com/repos/andymckay/webextension-schema/pulls'
+    res = requests.post(
+        url,
+        json={
+            'title': 'add-ons robot updates from mozilla-central',
+            'head': 'addons-robot:changes-{}'.format(uid),
+            'base': 'master',
+            'body': 'Updates from mozilla-central on {}'.format(
+                datetime.datetime.today().date())
+        },
+        auth=(GITHUB_USERNAME, GITHUB_TOKEN)
+    )
+
+
+@contextlib.contextmanager
+def temp_chdir(path):
+    starting_directory = os.getcwd()
+    try:
+        log.info('cd {}'.format(path))
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(starting_directory)
+
+
+def setup():
+    with temp_chdir(project_root):
+        if not os.path.exists(source_root):
+            git('clone {}'.format(source_repo))
+
+    with temp_chdir(os.path.join(project_root, source_root)):
+        git('pull')
+
+    with temp_chdir(project_root):
+        if not os.path.exists(dest_root):
+            git('clone {}'.format(dest_repo))
+
+    with temp_chdir(os.path.join(project_root, dest_root)):
+        git('pull')
+        git('config user.name "Addons Robot"')
+        git('config user.email "addons-dev-automation+github@mozilla.com"')
+
+
+def copy_files():
+    for src_dir, dest_dir in dir_mapping:
+        for root, dirname, filenames in os.walk(src_dir):
+            for filename in filenames:
+                full = os.path.join(root, filename)
+                if full.endswith('.json'):
+                    shutil.copy(full, dest_dir)
+
+
+def make_commit(target, uid):
+    with temp_chdir(os.path.join(project_root, dest_root)):
+        stdout = git('status --porcelain')
+        changed = False
+
+        for line in stdout:
+            if line.startswith('??'):
+                git('add {}'.format(line[3:]))
+            if line.startswith(' M'):
+                git('add {}'.format(line[3:]))
+            changed = True
+
+        if changed:
+            git('branch changes-{}'.format(uid))
+            git('commit -m "changes from addons-robot {}" -a'.format(uid))
+            git('push {} changes-{}'.format(target, uid))
+            return True
+
+    return False
+
+
+if __name__ == '__main__':
+    uid = str(time.time())
+    setup()
+    copy_files()
+    commit = make_commit('addons-robot', uid)
+    if commit:
+        make_pull_request(commit, uid)
